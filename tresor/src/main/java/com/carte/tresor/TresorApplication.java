1b756a769bc03418301f2e7e16b3036f8460d146
elouhichi <108538201+elouhichi@users.noreply.github.com>
2024-03-15 13:21:45 +0100

    socle technique

---
+++ tresor/src/main/java/com/carte/tresor/TresorApplication.java
@@ -0,0 +1,13 @@
+package com.carte.tresor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TresorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TresorApplication.class, args);
+	}
+
+}
---
+++ tresor/src/test/java/com/carte/tresor/TresorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carte.tresor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TresorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
